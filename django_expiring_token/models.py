4006ad799a151f76efbefcdabc1416c0c2e04a6d
KlemziStrajh <klemen.strajhar@gmail.com>
2018-12-22 13:42:16 +0100

    Removed secrets module
    Previous python versions do not support secrets module.
    New token generator now generates a token with a length 50
    hex characters.

--- django_expiring_token/models.py
+++ django_expiring_token/models.py
@@ -1,15 +1,16 @@
-import secrets
+import binascii
+import os
 
-from django.utils import timezone
-from rest_framework.authtoken.models import Token
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext as _
+from rest_framework.authtoken.models import Token
 
 from django_expiring_token.settings import custom_settings
 
 
 class ExpiringToken(Token):
-    key = models.CharField(_("Key"), max_length=45, primary_key=True)
+    key = models.CharField(_("Key"), max_length=50, primary_key=True)
     expires = models.DateTimeField(_("Expires in"), )
 
     def save(self, *args, **kwargs):
@@ -20,4 +21,4 @@ class ExpiringToken(Token):
         return super(ExpiringToken, self).save(*args, **kwargs)
 
     def generate_key(self):
-        return secrets.token_urlsafe(32)
+        return binascii.hexlify(os.urandom(25)).decode()
